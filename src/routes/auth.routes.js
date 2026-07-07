5e702bc5c4d5561f12ad55d176153c7050b66416
kamalprassath30 <kamalprasath2003@gmail.com>
2026-07-07 20:33:18 +0530

    changes Github auth

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -12,11 +12,46 @@ router.get("/github", (req, res) => {
   res.redirect(githubAuthUrl);
 });
 
+// router.get("/github/callback", async (req, res) => {
+//   const code = req.query.code;
+
+//   try {
+//     const response = await axios.post(
+//       "https://github.com/login/oauth/access_token",
+//       {
+//         client_id: process.env.GITHUB_CLIENT_ID,
+//         client_secret: process.env.GITHUB_CLIENT_SECRET,
+//         code,
+//       },
+//       {
+//         headers: {
+//           Accept: "application/json",
+//         },
+//       },
+//     );
+
+//     const accessToken = response.data.access_token;
+
+//     res.json({
+//       success: true,
+//       accessToken,
+//     });
+//   } catch (err) {
+//     console.error(err.response?.data || err.message);
+
+//     res.status(500).json({
+//       success: false,
+//       message: "GitHub OAuth failed",
+//     });
+//   }
+// });
+
 router.get("/github/callback", async (req, res) => {
   const code = req.query.code;
 
   try {
-    const response = await axios.post(
+    // Exchange code for access token
+    const tokenResponse = await axios.post(
       "https://github.com/login/oauth/access_token",
       {
         client_id: process.env.GITHUB_CLIENT_ID,
@@ -30,11 +65,32 @@ router.get("/github/callback", async (req, res) => {
       },
     );
 
-    const accessToken = response.data.access_token;
+    const accessToken = tokenResponse.data.access_token;
+
+    // Fetch logged-in user
+    const userResponse = await axios.get("https://api.github.com/user", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+
+    // Fetch user's repositories
+    const repoResponse = await axios.get("https://api.github.com/user/repos", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
 
     res.json({
       success: true,
-      accessToken,
+      user: {
+        login: userResponse.data.login,
+        name: userResponse.data.name,
+      },
+      repositories: repoResponse.data.map((repo) => ({
+        name: repo.name,
+        full_name: repo.full_name,
+      })),
     });
   } catch (err) {
     console.error(err.response?.data || err.message);
