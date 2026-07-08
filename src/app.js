7b7d6083da3de02756a6ebac91cac2aa3231312b
kamalprassath30 <kamalprasath2003@gmail.com>
2026-07-08 11:25:02 +0530

    added get logs

--- src/app.js
+++ src/app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const webhookroutes = require("./routes/webhook.routes");
 const authRoutes = require("./routes/auth.routes");
+const logRoutes = require("./routes/log.routes");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/webhook", webhookroutes);
 app.use("/auth", authRoutes);
+app.use("/logs", logRoutes);
 
 app.get("/", (req, res) => {
   res.send("Github Automation Bot API is running!");
---
+++ src/routes/log.routes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const pool = require("../database/db");
+
+const router = express.Router();
+
+router.get("/", async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT *
+      FROM bot_logs
+      ORDER BY created_at DESC
+    `);
+
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch logs",
+    });
+  }
+});
+
+module.exports = router;
